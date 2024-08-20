f84e19624e8ed6258e5989d55233f78e24c2ccd4
Gleb <PutintsevHO@amofintech.com>
2024-08-20 04:47:00 +0300

    Changed workflow

--- src/main/java/com/realworld/springmongo/security/SecurityConfig.java
+++ src/main/java/com/realworld/springmongo/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.realworld.springmongo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,9 +14,9 @@ import org.springframework.security.web.server.authentication.AuthenticationWebF
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationWebFilter webFilter, EndpointsSecurityConfig endpointsConfig) {
-        var authorizeExchange = http.authorizeExchange();
-        return endpointsConfig.apply(authorizeExchange)
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationWebFilter webFilter) {
+        return http.authorizeExchange()
+                .pathMatchers("/**").permitAll() // Allow all requests without authentication
                 .and()
                 .addFilterAt(webFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .httpBasic().disable()
@@ -28,22 +27,9 @@ public class SecurityConfig {
                 .build();
     }
 
-    /**
-     * Moving endpoints config to particular interface allow to change endpoints in tests.
-     */
-    @Bean
-    EndpointsSecurityConfig endpointsConfig() {
-        return http -> http
-                 
-                .pathMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/profiles/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/articles/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/tags/**").permitAll()
-                .anyExchange().authenticated();
-    }
-
     @FunctionalInterface
     public interface EndpointsSecurityConfig {
         AuthorizeExchangeSpec apply(AuthorizeExchangeSpec http);
     }
 }
+
