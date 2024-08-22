a1c645d833a84f8870facad49e9585ec73d56b73
Gleb <PutintsevHO@amofintech.com>
2024-08-22 12:23:32 +0300

    Changed workflow

--- src/main/java/com/realworld/springmongo/security/SecurityConfig.java
+++ src/main/java/com/realworld/springmongo/security/SecurityConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,13 +17,13 @@ import java.util.Arrays;
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationWebFilter webFilter, EndpointsSecurityConfig endpointsConfig) {
-        var authorizeExchange = http.authorizeExchange();
-        return endpointsConfig.apply(authorizeExchange)
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange()
+                .anyExchange().permitAll() // Allows all requests without authentication
                 .and()
-                .addFilterAt(webFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .httpBasic().disable()
-                .cors().configurationSource(corsConfigurationSource()) // Активуємо CORS
+                .cors().configurationSource(corsConfigurationSource()) // Enables CORS
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
@@ -44,21 +42,6 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    @Bean
-    EndpointsSecurityConfig endpointsConfig() {
-        return http -> http
-                .pathMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/profiles/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/articles/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/api/tags/**").permitAll()
-                .anyExchange().authenticated();
-    }
-
-    @FunctionalInterface
-    public interface EndpointsSecurityConfig {
-        ServerHttpSecurity.AuthorizeExchangeSpec apply(ServerHttpSecurity.AuthorizeExchangeSpec http);
     }
 }
 
